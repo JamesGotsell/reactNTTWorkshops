813ef03911708289ab9910d2e5787d16029e4e83
DyllanMarais <89013099+DyllanMarais@users.noreply.github.com>
2021-12-07 16:11:38 +0000

    API

--- lesson_1/lesson-1-no-jsx/src/Components/RickAndMorty.js
+++ lesson_1/lesson-1-no-jsx/src/Components/RickAndMorty.js
@@ -3,18 +3,26 @@ import React, {Component, component} from 'react'
 class RickAndMorty extends Component {
     constructor(props) {
         super(props);
-        this.state={};
+        //this.state={posts:[]};
     }
     componentDidMount(){
         //logic to call API
-        fetch('https://rickandmortyapi.com/api').then(resp=>resp.json())
-    .then(resp=>console.log(resp))
+        // fetch('https://rickandmortyapi.com/api').then(resp=>resp.json())
+        //     .then(resp=>this.setState({posts:resp}))
+        // fetch('https://rickandmortyapi.com/api/character').then(resp=>resp.json())
+        // .then(resp=>this.setState({posts:resp}))
+        // fetch('https://rickandmortyapi.com/api/location').then(resp=>resp.json())
+        // .then(resp=>this.setState({posts:resp}))
+        const result = fetch('https://rickandmortyapi.com/api/episode').then(resp=>resp.json())
+        .then(data => { return data })
+        console.log(result)
     }
 
     render() {
         return(
            <div>
          <h1>Rick And Morty</h1>
+         |
         </div>
         )
     }
