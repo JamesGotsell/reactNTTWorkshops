a07c8914f791074d801a2ca84cf42f36b81ffb75
James Gotsell <j.e.gotsell@gmail.com>
2021-12-07 16:28:33 +0000

    chore: got the component display values from api

--- lesson_1/lesson-1-no-jsx/src/Components/RickAndMorty.js
+++ lesson_1/lesson-1-no-jsx/src/Components/RickAndMorty.js
@@ -1,20 +1,36 @@
-import React, {Component, component} from 'react'
+import React, {Component } from 'react'
 
 class RickAndMorty extends Component {
     constructor(props) {
         super(props);
-        this.state={};
+
+        this.state = {
+            characters: []
+        };
     }
     componentDidMount(){
-        //logic to call API
-        fetch('https://rickandmortyapi.com/api').then(resp=>resp.json())
-    .then(resp=>console.log(resp))
+         fetch('https://rickandmortyapi.com/api/character').then((resp) => resp.json())
+        .then(resp => { 
+            this.setState({
+                characters: resp.results
+            })
+        })
+        
+    }
+
+    logState = () => {
+        console.log(this.state.characters)
     }
 
     render() {
         return(
            <div>
          <h1>Rick And Morty</h1>
+         <button onClick={() => this.logState()} >check state </button>
+        create a character card component - functional component - pass state as props 
+        {
+            this.state.characters.map((item) => { return <><p>{item.name} </p></>})
+        }
         </div>
         )
     }
